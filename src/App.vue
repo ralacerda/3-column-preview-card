5166900de785a5384a5d56430756c58bd267b786
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-11-11 18:03:51 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
+<template>
+  <main>
+    <h1>Simple count</h1>
+    <button @click="count++">Increase the count</button>
+    <p>{{ count }}</p>
+  </main>
+</template>
